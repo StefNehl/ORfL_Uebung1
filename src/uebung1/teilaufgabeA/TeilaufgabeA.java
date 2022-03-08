e138d5d303ed6767b2c8bb1a642a362d049d1096
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-03-08 14:04:07 +0100

    Started with implementation

---
+++ src/uebung1/teilaufgabeA/TeilaufgabeA.java
@@ -0,0 +1,22 @@
+package uebung1.teilaufgabeA;
+
+import org.ejml.simple.SimpleMatrix;
+
+public class TeilaufgabeA
+{
+    public static void main(String[] args)
+    {
+        final int NUMBER_OF_ITERATIONS = 30;
+        final double WIN_PROBABILITY = 0.1;
+
+        var probabilityArray = new double[40][40];
+
+        for(int i = 0; i < probabilityArray.length; i++)
+        {
+            for(int j = 0; j < probabilityArray[0].length; j++)
+            {
+
+            }
+        }
+    }
+}
