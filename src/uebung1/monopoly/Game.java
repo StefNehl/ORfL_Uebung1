2003dea47aad3e64af71ea220dce46b8d46855c4
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-03-08 13:43:12 +0100

    Added Player

---
+++ src/uebung1/monopoly/Game.java
@@ -0,0 +1,11 @@
+package uebung1.monopoly;
+
+public class Game {
+
+    private Board _board;
+    private Dice[] _dices;
+    private Player[] _players;
+
+
+
+}
---
+++ src/uebung1/monopoly/Player.java
@@ -0,0 +1,11 @@
+package uebung1.monopoly;
+
+public class Player
+{
+    private int _fieldIndex;
+
+    public Player(int initFieldIndex)
+    {
+
+    }
+}
