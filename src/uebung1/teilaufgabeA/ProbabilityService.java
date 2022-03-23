9ddc953b4ac0a78701195bf032dc4fa014186f78
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-03-23 20:28:25 +0100

    Started with implementation

---
+++ src/uebung1/teilaufgabeA/ProbabilityService.java
@@ -0,0 +1,57 @@
+package uebung1.teilaufgabeA;
+
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.simple.SimpleMatrix;
+
+public class ProbabilityService
+{
+    private double[] propArray;
+
+    public ProbabilityService()
+    {
+        var faces = 6;
+        var numberOfDices = 2;
+        var numberOfOutcomes = 36;
+
+        var propMatrix = new SimpleMatrix(faces, faces);
+
+        for(int c = 0; c < propMatrix.numCols(); c++)
+        {
+            for(int r = 0; r < propMatrix.numRows(); r++)
+            {
+                var valueToSet = r+1+c+1;
+                propMatrix.set(r, c, valueToSet);
+            }
+        }
+
+        var matrixData = ((DMatrixRMaj)propMatrix.getMatrix()).getData();
+        propArray = new double[numberOfDices * faces];
+        for(int i = 0; i < propArray.length; i++)
+        {
+            var diceNumber = (double)i + 1;
+            var count = 0;
+
+            for(int j = 0; j < matrixData.length; j++)
+            {
+                if(matrixData[j] == diceNumber)
+                    count++;
+            }
+
+            propArray[i] = (double) count / numberOfOutcomes;
+        }
+
+    }
+    public double[] getDiceProbabilityArray()
+    {
+        return propArray;
+    }
+
+    public double getDiceProbabilityForNumber(int number)
+    {
+        if(number < 2 || number >= 12)
+            return 0;
+
+        var index = number - 1;
+        return propArray[index];
+    }
+}
--- src/uebung1/teilaufgabeA/TeilaufgabeA.java
+++ src/uebung1/teilaufgabeA/TeilaufgabeA.java
@@ -1,22 +1,80 @@
 package uebung1.teilaufgabeA;
 
+import org.ejml.dense.fixed.CommonOps_DDF5;
 import org.ejml.simple.SimpleMatrix;
 
 public class TeilaufgabeA
 {
     public static void main(String[] args)
     {
+        testProbService();
         final int NUMBER_OF_ITERATIONS = 30;
         final double WIN_PROBABILITY = 0.1;
 
-        var probabilityArray = new double[40][40];
+        var probService = new ProbabilityService();
 
-        for(int i = 0; i < probabilityArray.length; i++)
+        var probabilityMatrix = new SimpleMatrix(10, 10);
+
+        for(int c = 0; c < probabilityMatrix.numCols(); c++)
         {
-            for(int j = 0; j < probabilityArray[0].length; j++)
+            for(int r = 0; r < probabilityMatrix.numRows(); r++)
             {
+                if(r <= c)
+                {
 
+                }
+                else
+                {
+                    var diceNumber = r - c;
+                    var numberProb = probService.getDiceProbabilityForNumber(diceNumber);
+                    probabilityMatrix.set(r, c, numberProb);
+                }
             }
         }
+
+        System.out.println(probabilityMatrix);
+        checkMarkovMatrix(probabilityMatrix);
+
+        var result = probabilityMatrix.copy();
+
+        for (int i = 0; i < 30 ; i++)
+        {
+            result = result.mult(probabilityMatrix);
+        }
+
+        System.out.println(result);
     }
+
+    public static void testProbService()
+    {
+        var service = new ProbabilityService();
+        var probArray = service.getDiceProbabilityArray();
+
+        System.out.println("Prob Array");
+        System.out.print("[ ");
+        for(int i = 0; i < probArray.length; i++)
+        {
+            System.out.print(probArray[i] + ", ");
+        }
+        System.out.println(" ]");
+
+        System.out.println(service.getDiceProbabilityForNumber(7));
+        System.out.println(service.getDiceProbabilityForNumber(1));
+        System.out.println(service.getDiceProbabilityForNumber(13));
+    }
+
+    public static void checkMarkovMatrix(SimpleMatrix matrix)
+    {
+        for(int r = 0; r < matrix.numRows(); r++)
+        {
+            var columnSum = 0.0;
+            for(int c = 0; c < matrix.numCols(); c++)
+            {
+                columnSum += matrix.get(r, c);
+            }
+            if(columnSum != 1.0)
+                System.out.println("Error in r: " + r + " Sum was: " + columnSum);
+        }
+    }
+
 }
